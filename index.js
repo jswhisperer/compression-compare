dcdffd7096668eae51fd27897b88a15cf64f33ba
Gregory JSWhisperer <hello@jswhisperer.uk>
2025-05-09 15:15:08 +0100

    move it to the browser
    remove node_modules dep

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-import { compress, decompress, init } from '@bokuweb/zstd-wasm';
-import { ZstdInit } from '@oneidentity/zstd-js';
-import { CompressionStream, DecompressionStream } from '@ungap/compression-stream';
-import byteSize from 'byte-size';
-import LZString from 'lz-string';
+// import { compress, decompress, init } from '@bokuweb/zstd-wasm';
+// import { ZstdInit } from '@oneidentity/zstd-js';
+// import { CompressionStream, DecompressionStream } from '@ungap/compression-stream';
+// import byteSize from 'byte-size';
+// import LZString from 'lz-string';
 
-const sampleArr = Array.from("hi".repeat(9999)).toString();
+import {Buffer} from 'https://cdn.jsdelivr.net/npm/buffer@6.0.3/+esm'
+import { compress, decompress, init } from 'https://cdn.jsdelivr.net/npm/@bokuweb/zstd-wasm@0.0.27/+esm'
+import { ZstdInit } from'https://cdn.jsdelivr.net/npm/@oneidentity/zstd-js@1.0.3/+esm'
+import { CompressionStream, DecompressionStream } from 'https://cdn.jsdelivr.net/npm/@ungap/compression-stream@0.1.0/+esm'
+import byteSize from 'https://cdn.jsdelivr.net/npm/byte-size@7.0.0/+esm'
+import LZString from 'https://cdn.jsdelivr.net/npm/lz-string@1.4.4/+esm'
+
+
+
+const sampleData = Array.from("hi".repeat(999999))
 
 
 const fileSizeReadable = (fileSizeInBytes) => {
@@ -14,17 +23,17 @@ const fileSizeReadable = (fileSizeInBytes) => {
 
 
 (async () => {
-  await init();
-  const Buffered = Buffer.from(sampleArr)
+  await init('./zstd.wasm');
+  const Buffered = Buffer.from(sampleData.toString())
   const compressed = compress(Buffered, 10);
   const res = decompress(compressed);
   const response = Buffer.from(res).toString();
-  var compressedLZ = LZString.compress(sampleArr);
+  var compressedLZ = LZString.compress(sampleData.toString());
 
   console.table({
-    "uncompressed": fileSizeReadable(Buffered.byteLength), "compressed": fileSizeReadable(compressed.byteLength),
+    "zstd-wasm uncompressed": fileSizeReadable(Buffered.byteLength), "zstd-wasm compressed": fileSizeReadable(compressed.byteLength),
     "LZ compressed": fileSizeReadable(compressedLZ.length),
-    "are they equal?": sampleArr === response});
+    "are they equal?": sampleData.toString() === response});
 })();
 
 
@@ -33,14 +42,9 @@ ZstdInit().then(({ZstdSimple, ZstdStream}) => {
   // Create some sample data to compress
   // const data  = new Uint8Array(Array.from("hi".repeat(9999)).toString());
 
-  const data = new Uint8Array(Array.from("hi".repeat(9999)));
+  const data = new Uint8Array(sampleData)
 
   const compressionLevel = 20;
-  const doCheckSum = true;
-  /*
-   * The required parameter is the data
-   * It must be a Uint8Array
-   * */
 
   const compressedStreamData = ZstdStream.compress(data, compressionLevel);
   const decompressedStreamData = ZstdStream.decompress(compressedStreamData);
@@ -99,7 +103,7 @@ async function compressAPI(str) {
    return new TextDecoder().decode(stringBytes);
  }
 
-const compressedBytes = await compressAPI(sampleArr);
+const compressedBytes = await compressAPI(sampleData.toString());
 const decompressedBytes = await decompressGzip(compressedBytes);
 console.table(
-    {"gzip compressed": fileSizeReadable(compressedBytes.length), "decompressed": fileSizeReadable(decompressedBytes.length)});
+    {"gzip uncompressed": fileSizeReadable(decompressedBytes.length), "gzip compressed": fileSizeReadable(compressedBytes.length), });
